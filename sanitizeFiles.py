92f0b70bada23c830c620b977c053b45e25bd571
Davide Della Casa <davidedc@gmail.com>
2020-12-14 14:58:20 +0000

    adding different sorts of printouts

--- sanitizeFiles.py
+++ sanitizeFiles.py
@@ -128,6 +128,30 @@ listOfFiles = [unicode(ntpath.basename(x), fse) for x in glob.glob(os.path.normp
 
 
 for eachFile in listOfFiles:
-	sanitized = sanitize(eachFile)
-	if sanitized != eachFile:
-		print(eachFile + " -> " + sanitized)
+    sanitized = sanitize(eachFile)
+    if sanitized != eachFile:
+        #print(eachFile + " -> " + sanitized)
+        print(sanitized)
+
+    '''
+    # USE THIS TO CHECK any remaining unexpected chars
+    if not re.match(r'^[A-Za-z0-9-]+$', sanitized):
+        print(sanitized)
+    '''
+
+    '''
+    if re.match(r'.*-s-', sanitized):
+        print(sanitized)
+
+    if re.match(r'.*-re-', sanitized):
+        print(sanitized)
+
+    if re.match(r'.*-ve-', sanitized):
+        print(sanitized)
+
+    if re.match(r'.*-ll-', sanitized):
+        print(sanitized)
+
+    if re.match(r'.*-t-', sanitized):
+        print(sanitized)
+    '''
