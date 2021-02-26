3dfad9ca8c15d6c1d2b7275f6560f9c3436cdbf9
Davide Della Casa <davidedc@gmail.com>
2021-02-26 22:26:18 +0000

    draft of script to create noteS for several images (i.e. one note for each image)

---
+++ download-google-images/create-one-note-around-each-image.py
@@ -0,0 +1,42 @@
+# coding=utf-8
+
+# Example:
+#   python create-one-note-around-each-image.py simple_images/bear/* -v downloads
+
+import sys
+import os
+import ntpath
+import codecs
+import argparse
+
+# enables to import from upper directory
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+
+from helper_routines import checkPath
+from helper_routines import getFileNames
+from helper_routines import bearEscapeDirectoryName
+
+parser = argparse.ArgumentParser(description="My parser")
+parser.add_argument('imageFiles', nargs='+')
+parser.add_argument('-v', '--vault-path')
+args = parser.parse_args()
+
+notesPath = os.path.join(args.vault_path, '') # add trailing slash if it's not there
+
+
+fse = sys.getfilesystemencoding()
+
+for f in args.imageFiles:
+    print(f)
+
+    eachImageFileName = unicode(ntpath.basename(os.path.normpath(f)), fse)
+    if eachImageFileName == ".DS_Store":
+        continue
+    
+    eachImageFileName_noExtension = os.path.splitext(eachImageFileName)[0]
+    noteContents = "# " + eachImageFileName_noExtension + "\n\n![](assets/" + bearEscapeDirectoryName(eachImageFileName_noExtension) + "/" + bearEscapeDirectoryName(eachImageFileName) +")"
+
+    with codecs.open(notesPath + eachImageFileName_noExtension + ".md", 'w', encoding='utf-8') as fileW:
+        fileW.write(noteContents)
+        fileW.close()
+
