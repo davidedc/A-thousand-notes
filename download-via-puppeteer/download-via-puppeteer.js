c919a62020265265cd5a7207ca34663c0e3ea0ad
Davide Della Casa <davidedc@gmail.com>
2021-03-03 21:37:02 +0000

    Update download-via-puppeteer.js

--- download-via-puppeteer/download-via-puppeteer.js
+++ download-via-puppeteer/download-via-puppeteer.js
@@ -1,17 +1,39 @@
-// example:
-// node download-via-puppeteer.js "https://www.youtube.com/watch?v=DjeN1Ea8uns&feature=share"
+// Example:
+// node download-via-puppeteer.js "https://twitter.com/ntsutae/status/1367089088315068419"
 
 const puppeteer = require('puppeteer');
-
 
 var theArgs = process.argv.slice(2);
 
 
 (async () => {
-  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
-  const page = await browser.newPage();
-  await page.goto(theArgs[0]);
-  await page.screenshot({path: 'buddy-screenshot.png'});
 
+  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+
+  const options = { encoding: "base64", fullPage: false , path: 'buddy-screenshot.png'};
+  const page = await browser.newPage();
+  let buffer;
+
+    await page.setViewport({
+      width: 767,
+      height: 800,
+      deviceScaleFactor: 2,
+    });
+    const tUrl = `https://publish.twitter.com/?query=${theArgs[0]}&widget=Tweet`;
+    await page.goto(tUrl, { waitUntil: "networkidle2" });
+    await page.waitFor(3000);
+    await page.evaluate((sel) => {
+      const elem = document.querySelector(sel);
+      elem.parentNode.removeChild(elem);
+    }, "body #top");
+    const selector = "body #WidgetConfigurator-preview .twitter-tweet";
+    await page.waitForSelector(selector); // wait for the selector to load
+    const element = await page.$(selector); // declare a variable with an ElementHandle
+    buffer = await element.screenshot(options);
   await browser.close();
+
+
 })();
+
+
+
