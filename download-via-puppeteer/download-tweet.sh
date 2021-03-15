09d53ef7324bdc37a6d126399aab6b279bc288f1
Davide Della Casa <davidedc@gmail.com>
2021-03-15 21:16:35 +0000

    streamlining the pandoc step (only one pandoc step needed instead of two)

--- download-via-puppeteer/download-tweet.sh
+++ download-via-puppeteer/download-tweet.sh
@@ -2,4 +2,4 @@
 #   sh download-tweet.sh "https://twitter.com/ntsutae/status/1367089088315068419"
 
 theNoteName="note-tweet-$RANDOM$RANDOM"
-node download-via-puppeteer-2.js "https://twitter.com/ntsutae/status/1367089088315068419" $theNoteName | pandoc -f markdown_strict -t html | pandoc -f html --extract-media ./assets/$theNoteName  -t markdown_strict -o $theNoteName.md
+node download-via-puppeteer-2.js "https://twitter.com/ntsutae/status/1367089088315068419" $theNoteName | pandoc -f markdown_strict --extract-media ./assets/$theNoteName  -t markdown_strict -o $theNoteName.md
